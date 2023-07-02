6a57b9aeb1caf017cf5c6551d79761b4610abd7e
Shimon <shimonbhuiya02@gmail.com>
2023-07-03 01:37:48 +0600

    Firebase 3

--- app/src/main/java/com/example/shimon/ciscord/HomeFragment.kt
+++ app/src/main/java/com/example/shimon/ciscord/HomeFragment.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import coil.load
 import com.example.shimon.ciscord.databinding.FragmentHomeBinding
 import com.example.shimon.ciscord.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -24,14 +26,39 @@ class HomeFragment : Fragment(), UserAdapter.UserListener {
 
     var userList: MutableList<User> = mutableListOf()
 
+    private val auth = FirebaseAuth.getInstance()
+    private lateinit var firebaseUSer: FirebaseUser
+
+    private var currentUser: User? = null
+
+    var bundle = Bundle()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         userDb = FirebaseDatabase.getInstance().reference
+
+        FirebaseAuth.getInstance().currentUser?.let {
+
+            firebaseUSer = it
+
+        }
+
+        binding.profileImage.setOnClickListener {
+
+            currentUser?.let {
+                bundle.putString(ProfileFragment.USERID, it.userId)
+                findNavController().navigate(R.id.action_homeFragment_to_profileFragment,bundle)
+
+            }
+
+        }
+
+
+
         binding.logoutBtn.setOnClickListener {
-            val auth = FirebaseAuth.getInstance()
+
 
             auth.signOut().apply {
 
@@ -54,40 +81,49 @@ class HomeFragment : Fragment(), UserAdapter.UserListener {
 
     private fun getAllAvailableUser() {
 
-        userDb.child(DBNOTES.USER).addValueEventListener(
-            object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
+        userDb.child(DBNOTES.USER).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
 
 
-                    userList.clear()
-                    snapshot.children.forEach {
+                userList.clear()
+                snapshot.children.forEach {
 
-                        val user: User = it.getValue(User::class.java)!!
+                    val user: User = it.getValue(User::class.java)!!
 
+                    if (firebaseUSer.uid != user.userId) {
                         userList.add(user)
-
+                    } else {
+                        currentUser = user
+                        setprofile()
                     }
-                    adapter.submitList(userList)
-                }
 
-                override fun onCancelled(error: DatabaseError) {
 
                 }
+                adapter.submitList(userList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
 
 
-            })
+        })
 
+    }
+
+    private fun setprofile() {
+        currentUser?.let {
+            binding.profileImage.load("https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061959920.jpg")
+        }
     }
 
     override fun userItemClick(user: User) {
 
-        var bundle = Bundle()
+        Log.d("hf", "${user.userId}")
 
-        Log.d("hf","${user.userId}")
+        bundle.putString(ProfileFragment.USERID, user.userId)
 
-        bundle.putString(ProfileFragment.USERID,user.userId)
-
-        findNavController().navigate(R.id.action_homeFragment_to_profileFragment,bundle)
+        findNavController().navigate(R.id.action_homeFragment_to_profileFragment, bundle)
     }
 
 
--- app/src/main/java/com/example/shimon/ciscord/editProfileFragment.kt
+++ app/src/main/java/com/example/shimon/ciscord/editProfileFragment.kt
@@ -6,31 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.shimon.ciscord.databinding.FragmentProfileBinding
+import android.widget.Toast
+import com.example.shimon.ciscord.databinding.FragmentEditProfileBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
+class editProfileFragment : Fragment() {
 
-class ProfileFragment : Fragment() {
-    lateinit var binding: FragmentProfileBinding
+
+
+    lateinit var binding:FragmentEditProfileBinding
 
     lateinit var userDB: DatabaseReference
 
     private var userId = ""
 
-    private var bundle = Bundle()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding = FragmentEditProfileBinding.inflate(inflater,container,false)
+
 
         userDB = FirebaseDatabase.getInstance().reference
 
@@ -39,60 +39,51 @@ class ProfileFragment : Fragment() {
 
             userId = it
 
-            Log.d("profileFragment", "before get user by getUserByID: $it")
             getUserByID(it)
 
         }
 
-        FirebaseAuth.getInstance().currentUser?.let {
-            if (userId == it.uid) {
 
-                binding.chatWithUserBTn.text = edit
-            } else {
-                binding.chatWithUserBTn.text = chat
+        binding.saveBTN.setOnClickListener {
+
+            val userMap:MutableMap<String, Any> = mutableMapOf()
+
+            userMap["fullName"] = binding.editName.text.toString().trim()
+            userMap["bio"] = binding.editBio.text.toString().trim()
+
+            userDB.child(DBNOTES.USER).child(userId).updateChildren(userMap).addOnCompleteListener {
+
+                if (it.isSuccessful) {
+                    Toast.makeText(requireContext(), "Profile Updated ! ", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(requireContext(), "${it.exception?.message}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
             }
 
         }
-
-        binding.chatWithUserBTn.setOnClickListener {
-
-            if (binding.chatWithUserBTn.text == edit) {
-
-                bundle.putString(editProfileFragment.USERID, userId)
-                findNavController().navigate(
-                    R.id.action_profileFragment_to_editProfileFragment,
-                    bundle
-                )
-            }
-
-        }
-
-
-
-
 
         return binding.root
     }
 
-
     companion object {
         const val USERID = "user_id_key"
-        const val edit = "Edit Profile"
-        const val chat = "Lets chat"
     }
 
     private fun getUserByID(userId: String) {
 
-        Log.d("user", "$userId")
 
-        userDB.child(DBNOTES.USER).child(userId).addValueEventListener(object : ValueEventListener {
+
+        userDB.child(userId).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
                 snapshot.getValue(User::class.java)?.let {
 
                     binding.userEmail.text = it.email
-                    binding.bio.text = it.bio
-                    binding.fullName.text = it.fullName
+                    binding.editBio.setText(it.bio)
+                    binding.editName.setText(it.fullName)
 
 
                 }
@@ -107,6 +98,7 @@ class ProfileFragment : Fragment() {
 
 
     }
+
 
 
 }
