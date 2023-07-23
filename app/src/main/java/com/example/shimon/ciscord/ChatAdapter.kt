013e78d55d3b298f848a3f174951acce61a778aa
Shimon <shimonbhuiya02@gmail.com>
2023-07-24 00:08:46 +0600

    Chat conversation done

---
+++ app/src/main/java/com/example/shimon/ciscord/ChatAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.shimon.ciscord
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter(var userIdSelf: String, private val chatList: MutableList<TextMessage>) :
+RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+
+    private val Left: Int = 1
+    private val Right: Int = 2
+
+
+
+    class ChatViewHolder(var itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var messageTv: TextView = itemView.findViewById(R.id.chatTV)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+
+        return if (viewType == Right) {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.right_chat_item, parent, false)
+            ChatViewHolder(view)
+        } else {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.left_chat_item, parent, false)
+            ChatViewHolder(view)
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+       val message = chatList[position]
+
+        holder.messageTv.text = message.text
+
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        return if (chatList[position].senderId == userIdSelf) {
+
+            Right
+        } else {
+
+            Left
+
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+
+
+}
--- app/src/main/java/com/example/shimon/ciscord/ChatFragment.kt
+++ app/src/main/java/com/example/shimon/ciscord/ChatFragment.kt
@@ -6,12 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import coil.load
+import com.bumptech.glide.Glide
 import com.example.shimon.ciscord.databinding.FragmentChatBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.util.UUID
-
 
 class ChatFragment : Fragment() {
 
@@ -19,13 +25,18 @@ class ChatFragment : Fragment() {
     lateinit var chatDB: DatabaseReference
     lateinit var userSelfId: String
     lateinit var userSelfIdRemote: String
+    val chatList = mutableListOf<TextMessage>()
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentChatBinding.inflate(layoutInflater, container, false)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        layoutManager.stackFromEnd = true
+        binding.chatRCV.layoutManager = layoutManager
+
 
         requireArguments().getString(USERID)?.let {
             userSelfIdRemote = it
@@ -38,7 +49,67 @@ class ChatFragment : Fragment() {
             userSelfId = it.uid
         }
 
+        chatDB.child(DBNOTES.USER).child(userSelfIdRemote)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    snapshot.getValue(User::class.java)?.let {
+
+                        Glide.with(requireContext()).load(it.profileImage)
+                            .placeholder(R.drawable.placeholder).into(binding.profilePicture)
+                        binding.profileName.text = it.fullName
+                        binding.gmailTV.text = it.email
+
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+
+            })
+
+        allMessageShow()
+
+
+
         return binding.root
+    }
+
+    private fun allMessageShow() {
+        chatDB.child(DBNOTES.CHAT).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                chatList.clear()
+
+                snapshot.children.forEach { snap ->
+                    snap.getValue(TextMessage::class.java)?.let {
+                        if (it.senderId == userSelfId && it.receiverId == userSelfIdRemote ||
+                            it.senderId == userSelfIdRemote && it.receiverId == userSelfId){
+
+                            chatList.add(it)
+
+                        }
+
+                    }
+
+                }
+
+                val adapter = ChatAdapter(userSelfId,chatList)
+
+                binding.chatRCV.adapter = adapter
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+
+            }
+
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -63,8 +134,10 @@ class ChatFragment : Fragment() {
         textMessage.msgId = msgId
 
         chatDB.child(DBNOTES.CHAT).child(msgId).setValue(textMessage).addOnCompleteListener {
+
             if (it.isSuccessful) {
                 Toast.makeText(requireContext(), "Sent Successfully", Toast.LENGTH_SHORT).show()
+                binding.chatET.setText("")
             } else {
                 Toast.makeText(
                     requireContext(),
